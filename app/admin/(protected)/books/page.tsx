01e8b277a1c185560b35815a9b31e0356c9307b2
ahmedmg0maa <medoogomaa6@gmail.com>
2026-05-21 10:39:44 +0300

    Delete app/admin/(protected) directory

--- app/admin/(protected)/books/page.tsx
+++
@@ -1,8 +0,0 @@
-import { requireAdminPage } from "@/lib/admin-auth"
-import { BooksManager } from "@/components/admin/books-manager"
-
-export default async function AdminBooksPage() {
-  await requireAdminPage({ debugLabel: "/admin/books" })
-
-  return <BooksManager />
-}
--- app/admin/(protected)/courses/page.tsx
+++
@@ -1,8 +0,0 @@
-import { requireAdminPage } from "@/lib/admin-auth"
-import { CoursesManager } from "@/components/admin/courses-manager"
-
-export default async function AdminCoursesPage() {
-  await requireAdminPage({ debugLabel: "/admin/courses" })
-
-  return <CoursesManager />
-}
